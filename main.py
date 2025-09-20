ae5aa23343f635c225512dcd1d64a1b6d8567955
temic137 <68796483+temic137@users.noreply.github.com>
2025-09-20 23:47:54 +0200

    stuff

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ app =  FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://arduinobooc.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
